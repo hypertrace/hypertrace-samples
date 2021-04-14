423756d4bcf5ad23ce0da6679ba7ebbaa7de3f85
Ronak <ronak@traceable.ai>
2021-04-14 09:45:13 +0530

    fix: fix the generation of config.yaml file, URL expose

--- async-kafka-application/async-kafka-producer-httpservice/run.sh
+++ async-kafka-application/async-kafka-producer-httpservice/run.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
 set -eu
-ENDPOINT=${TRACE_EXPORTER_END_POINT:-"localhost:9411"}
-sed -i -e 's/TRACE_EXPORTER_END_POINT/'"${ENDPOINT}"'/g' config.yaml
+ENDPOINT=${TRACE_EXPORTER_END_POINT:-"http://localhost:9411/api/v2/spans"}
+NAME=${SERVICE_NAME:-"async-kafka-producer-httpservice"}
+
+# prepare config.yaml file
+text=''
+text=$text"service_name: ${NAME}\n"
+text=$text"reporting:\n"
+text=$text"  endpoint: ${ENDPOINT}\n"
+echo -e $text > config.yaml
+
 java -javaagent:/app/hypertrace-agent-all.jar -jar ./libs/async-kafka-producer-httpservice-1.0.jar
